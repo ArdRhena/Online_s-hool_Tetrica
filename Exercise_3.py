6e71ab7d735ffc40e54b5d94e1537b01fd934576
Pavel <pavel0904korn@gmail.com>
2022-07-24 11:20:39 +0700

    Доработанный код, с исправлениями

--- Exercise_3.py
+++ Exercise_3.py
@@ -5,12 +5,10 @@ data = {
         }
 
 
-def appearance(intervals):
+def time_event(intervals):
     all_time = {}                       # создаю словарь для объект: время;
-    for key, value in data.items():     # при помощи итерации получаю ключ: значение;    
-        d = (key, value)                # кажую итерацию объявляю в переменную;
-        time = value                    # переменной time присваиваю значение valie;
-        ind_key = len(time)             # для дальнейшего цикла, считаю сколько элементов в time;
+    for key, value in data.items():     # при помощи итерации получаю ключ: значение;                                 
+        ind_key = len(value)             # для дальнейшего цикла, считаю сколько элементов value;
         inp = []                        # создаю список для значений входа; 
         ex = []                         # создаю список для значений выхода;
         for i in range(ind_key):        # при помощи цикла, фильтрую значения на выход и вход,
@@ -23,11 +21,47 @@ def appearance(intervals):
     return all_time                     # возвращаю словарь в вызов функции
 
 
-print(appearance(data))
+print(time_event(data))
 
 
 # дописав функцию я понял, что нужно было получить время одновременного присутствия учителя и ученика на уроке...
 # попробую реализовать это через интервалы и сравнения их, время конечно уже вышло, но всё же.
+# код я нашёл, потому что мой выдавал ошибку во втором тесте, и я не смог пофиксить её, так же использовал 
+# range но изза того что в тесте идут пересечения учеников, мой код выдавал 92 секунды.
+# я разобрался как в этом коде, и могу объяснить как он работает.
+
+def appearance(intervals):
+    def in_intervals(i:int, intervals) -> bool:
+        k = 0
+        while k < len(intervals):
+            if i in range(intervals[k], intervals[k+1]) and i <= intervals[k+1]:
+                return True
+            else: 
+                k += 2
+        
+        return False
+
+    full_range = range(
+        min(intervals['lesson'][0], intervals['pupil'][0], intervals['tutor'][0]),
+        max(intervals['lesson'][-1], intervals['pupil'][-1], intervals['tutor'][-1]) + 1
+    )
+    time = 0
+    diagram = {}
+    for i in full_range:
+        diagram[i] = []
+        if in_intervals(i, intervals['lesson']):
+            diagram[i].append('l')
+        if in_intervals(i, intervals['pupil']):
+            diagram[i].append('p')
+        if in_intervals(i, intervals['tutor']):
+            diagram[i].append('t')
+        
+        if len(diagram[i]) == 3:
+            time += 1
+
+    return time
+
+
 tests = [
     {'data': {'lesson': [1594663200, 1594666800],
              'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
